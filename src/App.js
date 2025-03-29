cad5d1d652f56529f4afc504688781edd6a4b639
Jasmine Castillo <jascas0224@students.ecpi.edu>
2025-03-28 21:01:35 -0400

    Added a quote and image.

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import './App.css';
 import Counter from './components/Counter';
 import TypeText from './components/TypeText';
 import ToDoList from './components/ToDoList';
+import React from 'react';
+import Cliffview from './Cliffview.PNG';
 
 function App() {
   return (
@@ -9,8 +11,10 @@ function App() {
       <Counter />
       <TypeText />
       <ToDoList />
+      <h3 className="img-aligntop">If you're not living on the edge, you can't see the view...</h3>
+      <img src={Cliffview} alt="The view" className="img-align" />
       <p className="footer-name">Jasmine Castillo</p>
-      <p className="footer-date">3/23/25</p>
+      <p className="footer-date">3/28/25</p>
     </div>
   );
 }
